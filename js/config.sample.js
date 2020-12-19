67ee7040e8fc199c5adf91ff6ba50399704fac13
ShevAbam <shevabam@gmail.com>
2020-12-19 21:02:08 +0100

    Redesign recovery of RSS feeds

---
+++ js/config.sample.js
@@ -0,0 +1,3 @@
+const _CONFIG_ = {
+    'api_token': ''
+}
--- js/functions.js
+++ js/functions.js
@@ -1,246 +1,62 @@
+
 /**
- * Get RSS feeds URLs
+ * Do XHR GET Request
+ */
+function getJSON(url, callback) {
+
+    var token = _CONFIG_.api_token;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+    xhr.responseType = 'json';
+
+    xhr.onload = function() {
+
+        var status = xhr.status;
+
+        if (status == 200) {
+            callback(xhr.response);
+        } else {
+            render('Unable to find feed');
+        }
+    }
+
+    xhr.send();
+}
+
+/**
+ * Fetch feeds
  */
 function getFeedsURLs(url, callback) {
+    if (url != 'undefined' && typeof url != 'undefined' && _CONFIG_.api_token != '') {
+        var params = {'from': 'extension_get-rss-feed-url'};
 
-    // Check if it's a Youtube URL (channel or user feed)
-    var ytRss = getYoutubeRss(url);
+        getJSON('https://get-rss-url-api.shevapps.fr/fetch.php?url='+url+'&params='+JSON.stringify(params), (response) =>  {
 
-    // Check if it's a Reddit URL
-    var rdRss = getRedditRss(url);
+            var feeds_urls = [];
 
-    // Check if it's a Kickstarter URL
-    var ksRss = getKickstarterRss(url);
+            if (response != null) {
 
-    if (ytRss !== false)
-    {
-        var feeds_urls = [];
-        
-        var feed = {
-            type: '',
-            url: ytRss,
-            title: ytRss,
-        };
-        
-        feeds_urls.push(feed);
+                for (var i = 0; i < response.datas.feeds.length; i++){
+                    var obj = response.datas.feeds[i];
 
-        callback(feeds_urls);
-    }
-    else if (rdRss !== false)
-    {
-        var feeds_urls = [];
-        
-        var feed = {
-            type: '',
-            url: rdRss,
-            title: rdRss,
-        };
-        
-        feeds_urls.push(feed);
+                    var feed_url = obj['url'];
+                    var feed_title = obj['title'];
 
-        callback(feeds_urls);
-    }
-    else if (ksRss !== false)
-    {
-        var feeds_urls = [];
-        
-        var feed = {
-            type: '',
-            url: ksRss,
-            title: ksRss,
-        };
-        
-        feeds_urls.push(feed);
+                    var feed = {
+                        url: feed_url,
+                        title: feed_title || feed_url
+                    };
 
-        callback(feeds_urls);
-    }
-    else
-    {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == XMLHttpRequest.DONE) {
-                var urlContent = xhr.responseText;
-
-                if (urlContent != '')
-                    document.getElementById('rss-feed-url_response').innerHTML = urlContent;
-
-                searchFeed(url, callback);
+                    feeds_urls.push(feed);
+                }
             }
-        }
-        xhr.open('GET', url, true);
-        xhr.send();
+
+            callback(feeds_urls);
+        });
     }
 }
 
-/**
- * Search RSS Feed in source code
- */
-function searchFeed(url, callback)
-{
-    if (document.getElementById('rss-feed-url_response').innerHTML != '')
-    {
-        var feeds_urls = [];
-        var types = [
-            'application/rss+xml',
-            'application/atom+xml',
-            'application/rdf+xml',
-            'application/rss',
-            'application/atom',
-            'application/rdf',
-            'text/rss+xml',
-            'text/atom+xml',
-            'text/rdf+xml',
-            'text/rss',
-            'text/atom',
-            'text/rdf'
-        ];
-
-        var links = document.getElementById('rss-feed-url_response').querySelectorAll("#rss-feed-url_response link[type]");
-
-        document.getElementById('rss-feed-url_response').innerHTML = '';
-
-        for (var i = 0; i < links.length; i++)
-        {
-            if (links[i].hasAttribute('type') && types.indexOf(links[i].getAttribute('type')) !== -1)
-            {
-                feed_url = links[i].getAttribute('href');
-
-                // If feed's url starts with "//"
-                if (feed_url.indexOf("//") == 0)
-                    feed_url = "http:" + feed_url;
-                // If feed's url starts with "/"
-                else if (feed_url.startsWith('/'))
-                    feed_url = url.split('/')[0] + '//' + url.split('/')[2] + feed_url;
-                // If feed's url starts with http or https
-                else if (/^(http|https):\/\//i.test(feed_url))
-                    feed_url = feed_url;
-                // If feed's has no slash
-                else if (!feed_url.match(/\//))
-                    feed_url = url.substr(0, url.lastIndexOf("/")) + '/' + feed_url;
-                else
-                    feed_url = url + "/" + feed_url.replace(/^\//g, '');
-
-                var feed = {
-                    type: links[i].getAttribute('type'),
-                    url: feed_url,
-                    title: links[i].getAttribute('title') || feed_url
-                };
-
-                feeds_urls.push(feed);
-            }
-        }
-
-        if (feeds_urls.length === 0)
-        {
-            var test_feed = tryToGetFeedURL(url);
-            if (test_feed !== null)
-                feeds_urls.push(test_feed);
-        }
-
-        callback(feeds_urls);
-    }
-    else
-    {
-        render('Unable to find feed');
-    }
-}
-
-
-
-/*
- * Get RSS feed URL of Youtube channel or user
- */
-function getYoutubeRss(tabUrl)
-{
-    // Check if Youtube URL
-    var regex = RegExp(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/gm);
-
-    var isYTUrl = regex.test(tabUrl);
-    
-    if (isYTUrl)
-    {
-        // Remove after "?"
-        tabUrl = tabUrl.split('?')[0];
-
-        var channel = '';
-
-        // https://www.youtube.com/channel/UCENv8pH4LkzvuSV_qHIcslg
-        // https://www.youtube.com/user/cestpassorcierftv/featured
-        if (tabUrl.split('channel/')[1])
-        {
-            channel = "channel_id=" + (tabUrl.split('channel/')[1]).split('/')[0]; 
-        }
-        else if (tabUrl.split('user/')[1])
-        {
-            channel = "user=" + (tabUrl.split('user/')[1]).split('/')[0];
-        }
-
-        if (channel != '')
-            return "https://www.youtube.com/feeds/videos.xml?" + channel;
-        else
-            return false;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-
-/*
- * Get RSS feed URL of a subreddit
- */
-function getRedditRss(tabUrl)
-{
-    // Check if subreddit URL
-    var regex = RegExp(/^(http(s)?:\/\/)?((w){3}.)?reddit\.com\/r\/(.+)/gm);
-
-    var isRedditUrl = regex.test(tabUrl);
-    
-    if (isRedditUrl)
-    {
-        // Remove last "/" if presents
-        var feedUrl = tabUrl.replace(/\/$/, '');
-        feedUrl = feedUrl + '.rss';
-
-        if (feedUrl != tabUrl)
-            return feedUrl;
-        else
-            return false;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-
-/*
- * Get RSS feed URL of kickstarter
- */
-function getKickstarterRss(tabUrl)
-{
-    // Check if subreddit URL
-    var regex = RegExp(/^(http(s)?:\/\/)?((w){3}.)?kickstarter\.com/gm);
-
-    var isKickstarterUrl = regex.test(tabUrl);
-    
-    if (isKickstarterUrl)
-    {
-        // Remove last "/" if presents
-        var feedUrl = tabUrl.replace(/\/$/, '');
-        feedUrl = feedUrl + '.atom';
-
-        if (feedUrl != tabUrl)
-            return feedUrl;
-        else
-            return false;
-    }
-    else
-    {
-        return false;
-    }
-}
 
 
 /**
@@ -250,6 +66,7 @@ function render(content)
 {
     document.getElementById('feeds').innerHTML = content;
 }
+
 
 /*
  * Copy to clipboard text with notification
@@ -302,54 +119,3 @@ function truncate(fullStr, strLen, separator) {
            fullStr.substr(fullStr.length - backChars);
 };
 
-
-/**
- * Attempt to find an RSS feed URL by providing a suffix
- */
-function tryToGetFeedURL(tabUrl) {
-    var url_datas = parseUrl(tabUrl);
-    var feed = null;
-    var isFound = false;
-
-    var tests = ['/feed', '/rss', '/rss.xml', '/feed.xml'];
-
-    for (var t = 0; t < tests.length; t++) {
-        if (isFound === false) {
-            var feed_url = url_datas.origin + tests[t];
-
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function(){
-                if (xhr.readyState == XMLHttpRequest.DONE) {
-                    return xhr.responseText;
-                }
-            };
-            xhr.open("GET", feed_url, false);
-            xhr.send();
-
-            var urlContent = xhr.responseText;
-
-            if (xhr.status != 404 && urlContent != '')
-            {
-                var oParser = new DOMParser();
-                var oDOM = oParser.parseFromString(urlContent, "application/xml");
-
-                var getRssTag = oDOM.getElementsByTagName('rss');
-                if (getRssTag.length > 0) {
-                    var getChannelTag = getRssTag['0'].getElementsByTagName('channel')
-
-                    if (getChannelTag.length > 0) {
-                        isFound = true;
-
-                        feed = {
-                            type: '',
-                            url: feed_url,
-                            title: feed_url
-                        };
-                    }
-                }
-            }
-        }
-    }
-
-    return feed;
-}
